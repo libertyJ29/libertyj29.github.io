dcb757d649931dce37c5a3fc96f3cd6b7909764c
Jonathan Geddes <libertyj29@gmail.com>
2019-05-29 12:19:21 +0100

    num2

--- keep/service-worker.js
+++ keep/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/keep/precache-manifest.215a43baeabb3b5335182aa0e40b47ac.js"
+  "/keep/precache-manifest.439d6349db5f1064bba23f470cdabd1c.js"
 );
 
 self.addEventListener('message', (event) => {
